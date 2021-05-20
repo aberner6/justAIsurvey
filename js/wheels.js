19ead09cc40b91b5caaa509377a2849dde85d6c0
Annelie <annelieberner@gmail.com>
2021-05-20 16:46:02 +0200

    scaled bars accrdg to global totals

--- js/wheels.js
+++ js/wheels.js
@@ -13,75 +13,159 @@ var svg = d3.select("#my_dataviz")
 var sdata;
 
 
-var numRows = 6;
+var numRows = 6; //this is how many question types there are for both sections
 var numCols = 6;
+
 const row = d3.scaleBand()
-  .domain(d3.range(numRows))
-  .range([leftMargin, width-leftMargin])
-  .padding(0.05);
+	.domain(d3.range(numRows))
+	.range([width/2, width-leftMargin])
+	.padding(0.05);
+
+const rowT = d3.scaleBand()
+	.domain(d3.range(numRows))
+	.range([leftMargin, width/2])
+	.padding(0.05);
 
 const col = d3.scaleBand()
-  .domain(d3.range(numCols))
-  .range([topMargin, height-topMargin])
-  .padding(0.05);
+	.domain(d3.range(numCols))
+	.range([topMargin, height-topMargin])
+	.padding(0.05);
 
+
+var originX = 0;
+var originY = 0;
+var outerCircleRadius = (height/2)/numRows; //scaled to page?
+var chairOriginX = originX + ((outerCircleRadius) * Math.sin(0));
+var chairOriginY = originY - ((outerCircleRadius) * Math.cos(0));
+
+var idVals = [];
+var theVals = [];
+var maxTotal = 0;
+var maxTheme, maxId = 0;
+var barScale = d3.scaleLinear()
+	.domain([0, maxTotal]) //minimum, maximum incoming total
+	.range([0, outerCircleRadius/2])
 d3.json("data_totals.json").then(function(data) {
 
 	console.log(data);
 	sdata = data.children;
 
-	var gid = svg.selectAll("g")
+	var gid = svg.selectAll(".gID")
 		.data(sdata[0].children)
 		.join("g")
 		.attr("class", function(d){
-			console.log(d);
 			return d.name;
 		})
 		.attr('transform', function(d,i){ 
-			console.log(i); 
+			// console.log(i); 
 			return `translate(${row(i)}, ${col(i)})` 
-			// return `translate(${col(i)}, ${row(i)})` 
-			// return `translate(${row(i)}, ${topMargin})` 
 		});
-
+	var outerCircle = gid
+		.append("circle")
+		.attr("cx",originX)
+		.attr("cy",originY)
+		.attr("r",outerCircleRadius) //if this is small then it is like an inner circle
+		.attr("fill","none")
+		.attr("stroke","lightgrey");
 	gid.append('text')
 		.attr('font-size', 6)
 		.attr('font-family', 'sans-serif')
 		.attr('x', 0)
 		.attr('y', 0)
 		.text(d => d.name);
-
-	var originX = 0;
-	var originY = 0;
-	var outerCircleRadius = 30; //scaled to page?
-	var chairOriginX = originX + ((outerCircleRadius) * Math.sin(0));
-	var chairOriginY = originY - ((outerCircleRadius) * Math.cos(0));
-
-	var outerCircle = gid
+	var gthe = svg.selectAll(".gTHE")
+		.data(sdata[1].children)
+		.join("g")
+		.attr("class", function(d){
+			// console.log(d);
+			return d.name;
+		})
+		.attr('transform', function(d,i){ 
+			// console.log(i); 
+			return `translate(${rowT(i)}, ${col(i)})` 
+		});
+	var outerCircle = gthe
 		.append("circle")
 		.attr("cx",originX)
 		.attr("cy",originY)
-		.attr("r",outerCircleRadius)
+		.attr("r",outerCircleRadius) //if this is small then it is like an inner circle
 		.attr("fill","none")
-		.attr("stroke","lightgrey")
+		.attr("stroke","lightgrey");
+	gthe.append('text')
+		.attr('font-size', 6)
+		.attr('font-family', 'sans-serif')
+		.attr('x', 0)
+		.attr('y', 0)
+		.text(d => d.name);
 
-  	gid.selectAll('rectID')
+
+
+
+
+
+
+  	var rectIdentity = gid.selectAll('.rectID')
 		.data(d => d.children)
 		.join('rect')
+		.attr("class",function(d){
+			idVals.push(d.value);
+			return "rectID";
+		})
 		.attr("width",5)
 		.attr("fill","pink")
 		.attr("stroke","blue")
-		.attr("height", function(d,i){
-			return 1+(d.value*outerCircleRadius/2); //scale this to min and max and outer radius size
+	    .attr("x", chairOriginX )
+	    .attr("y", chairOriginY )
+		.attr("transform", function(d,i){
+			return "rotate("+(10*i)+", 0, 0)";
+		}) 
+		.attr("height",0);
+	maxId = d3.max(idVals);
+  	var rectTheme = gthe.selectAll('.rectTHE')
+		.data(d => d.children)
+		.join('rect')
+		.attr("class",function(d){
+			theVals.push(d.value);
+			return "rectTHE"
 		})
+		.attr("width",5)
+		.attr("fill","pink")
+		.attr("stroke","blue")
 	    .attr("x", chairOriginX ) //- (chairWidth / 2)
 	    .attr("y", chairOriginY ) //- (chairWidth / 2)
 		.attr("transform", function(d,i){
-			// calculate angle more smartly
-			// 
-			return "rotate("+(10*i)+", 0, 0)";
+			return "rotate("+(10*i)+", 0, 0)";// calculate angle more smartly
 		}) 
+		.attr("height",0);
+	maxTheme = d3.max(theVals);
 
+	if(maxId>0 && maxTheme>0){
+		// console.log(maxTotal)
+		if(maxId>maxTheme){
+			maxTotal = maxId;
+			// console.log(maxTotal)
+		}else{
+			maxTotal = maxTheme;
+			// console.log(maxTotal)
+		}
+	}
+
+	if(maxTotal>0){
+		barScale.domain([0, maxTotal]) 
+
+		// console.log(maxTotal)
+		rectIdentity.transition().duration(4000).attr("height", function(d,i){
+			// console.log(d.value)
+			console.log(barScale(d.value))
+			return barScale(d.value);
+			// return 1+(d.value*outerCircleRadius/2); //scale this to min and max and outer radius size
+		})
+		rectTheme.transition().duration(4000).attr("height", function(d,i){
+			console.log(barScale(d.value))
+			return barScale(d.value);
+			// return 1+(d.value*outerCircleRadius/2); //scale this to min and max and outer radius size
+		})
+	}
 })
 
 
