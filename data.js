6e3d76f2af8dda6a59d478643a56b1ec93486ba3
danielstaleiny <danielstaleiny@tutanota.com>
2021-06-04 11:25:10 +0200

    sceleton of json

--- data.js
+++ data.js
@@ -9,10 +9,15 @@ const api = createClient(
 )
 
 const main = async () => {
-    let { data: data, error } = await api
+    let {
+        data: data,
+        error,
+        count,
+    } = await api
         .from('data')
         .select(
-            `id,response_id, created, email, q7,q70,q71,q65,q9,q30,q133,q149,q33,q139,q155,q35,q141,q157,q36,q142,q158,q14,q37,q143,q159,q51,q144,q160`
+            `id,response_id, created, email, q7,q70,q71,q65,q9,q30,q133,q149,q33,q139,q155,q35,q141,q157,q36,q142,q158,q14,q37,q143,q159,q51,q144,q160`,
+            { count: 'exact' }
         )
 
     if (error) {
@@ -20,8 +25,38 @@ const main = async () => {
         return
     }
 
-    const correct_format = data.map(reshapeData)
-    // console.log(correct_format)
+    const reshapedArr = data.map(reshapeData)
+
+    const result = {
+        name: '',
+        children: [
+            {
+                name: 'identity',
+                children: [
+                    { name: 'self-ethicist', children: [] },
+                    { name: 'others-ethicist', children: [] },
+                    { name: 'funding', children: [] },
+                    { name: 'years in field', children: [] },
+                    { name: 'education', children: [] },
+                    { name: 'career path', children: [] },
+                ],
+            },
+            {
+                name: 'theme',
+                children: [
+                    { name: 'topics', children: [] },
+                    { name: 'domain', children: [] },
+                    { name: 'outputs', children: [] },
+                    { name: 'audiences', children: [] },
+                    { name: 'collab type', children: [] },
+                    { name: 'collab field', children: [] },
+                ],
+            },
+            { responses: count },
+        ],
+    }
+
+    console.log(result)
 }
 
 main().catch(console.error)
